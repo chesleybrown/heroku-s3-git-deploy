831026c517263d1cc078c7c34baf0656cf18fa93
Chesley Brown <chezbrown@gmail.com>
2014-12-03 09:38:30 -0330

    Added first test

---
+++ test/app.spec.js
@@ -0,0 +1,31 @@
+var expect = require('chai').expect;
+var request = require('supertest');
+
+describe('App', function () {
+	var server, app, response;
+	
+	beforeEach(function () {
+		server = undefined;
+		app = undefined;
+		response = undefined;
+	});
+	beforeEach(function () {
+		app = require('../app')();
+	});
+	
+	describe('when calling an unknown url', function () {
+		beforeEach(function () {
+			response = request(app)
+				.get('/invalid/')
+			;
+		});
+		
+		it('should respond with not found', function (done) {
+			response.end(function (err, res) {
+				expect(res.status).to.equal(404);
+				expect(res.body).to.be.empty;
+				done();
+			});
+		});
+	});
+})
