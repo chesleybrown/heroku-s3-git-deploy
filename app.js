86c3240553cee8428eb44e0865020748aa04972b
Chesley Brown <chezbrown@gmail.com>
2014-12-03 11:15:21 -0330

    Verified index page aws s3 access with tests

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var AWS = require('aws-sdk');
 var pkgcloudContainerCopy = require('pkgcloud-container-copy');
 var when = require('when');
 
@@ -12,12 +13,15 @@ module.exports = function () {
 	app.enable('trust proxy');
 	
 	app.get('/', function (req, res) {
-		res.render('index', {
-			AWS_ACCESS_KEY_ID: Boolean(process.env.AWS_ACCESS_KEY_ID),
-			AWS_SECRET_ACCESS_KEY: Boolean(process.env.AWS_SECRET_ACCESS_KEY),
-			host: req.protocol + '://' + req.get('host'),
-			authenticated: false
-		})
+		var s3 = new AWS.S3();
+		s3.listBuckets(function (err, data) {
+			res.render('index', {
+				AWS_ACCESS_KEY_ID: Boolean(process.env.AWS_ACCESS_KEY_ID),
+				AWS_SECRET_ACCESS_KEY: Boolean(process.env.AWS_SECRET_ACCESS_KEY),
+				host: req.protocol + '://' + req.get('host'),
+				authenticated: (err) ? false : true
+			});
+		});
 	});
 	
 	app.get('/commit-hook', function (req, res) {
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,14 +1,12 @@
 var expect = require('chai').expect;
 var request = require('supertest');
+var AWS = require('aws-sdk');
 
 describe('Index', function () {
 	var app, response;
 	
 	before(function () {
 		app = require('../app')();
-	});
-	after(function () {
-		response = undefined;
 	});
 	
 	describe('when calling an unknown url', function () {
@@ -28,27 +26,125 @@ describe('Index', function () {
 		});
 	});
 	
-	describe('when accessing index page', function () {
-		before(function (done) {
-			request(app)
-				.get('/')
-				.end(function (err, res) {
-					response = res;
-					done();
-				})
-			;
-		});
-		
-		it('should respond with success', function () {
-			expect(response.status).to.equal(200);
-		});
-		it('should render', function () {
-			expect(response.text).to.contain('<!DOCTYPE html>');
-		});
-		it('should complain about configuration', function () {
-			expect(response.text).to.contain('App is running, but missing required configuration!');
-			expect(response.text).to.contain('You still need to set the <span class="label label-default">AWS_ACCESS_KEY_ID</span> ENV variable.');
-			expect(response.text).to.contain('You still need to set the <span class="label label-default">AWS_SECRET_ACCESS_KEY</span> ENV variable.');
+	describe('when no environment set', function () {
+		describe('and accessing index page', function () {
+			before(function (done) {
+				AWS.S3 = function () {
+					return {
+						listBuckets: function (callback) {
+							callback(true);
+						}
+					};
+				};
+				request(app)
+					.get('/')
+					.end(function (err, res) {
+						response = res;
+						done();
+					})
+				;
+			});
+			
+			it('should respond with success', function () {
+				expect(response.status).to.equal(200);
+			});
+			it('should render', function () {
+				expect(response.text).to.contain('<!DOCTYPE html>');
+			});
+			it('should complain about configuration', function () {
+				expect(response.text).to.contain('App is running, but missing required configuration!');
+				expect(response.text).to.contain('You still need to set the <span class="label label-default">AWS_ACCESS_KEY_ID</span> ENV variable.');
+				expect(response.text).to.contain('You still need to set the <span class="label label-default">AWS_SECRET_ACCESS_KEY</span> ENV variable.');
+			});
 		});
 	});
+	
+	describe('when environment set', function () {
+		before(function () {
+			process.env.AWS_ACCESS_KEY_ID = 'access_key';
+			process.env.AWS_SECRET_ACCESS_KEY = 'secret_access_key';
+		});
+		
+		describe('and credentials are correct', function () {
+			before(function () {
+				AWS.S3 = function () {
+					return {
+						listBuckets: function (callback) {
+							callback(null);
+						}
+					};
+				};
+			});
+			
+			describe('and accessing index page', function () {
+				before(function (done) {
+					request(app)
+						.get('/')
+						.end(function (err, res) {
+							response = res;
+							done();
+						})
+					;
+				});
+				
+				it('should respond with success', function () {
+					expect(response.status).to.equal(200);
+				});
+				it('should render', function () {
+					expect(response.text).to.contain('<!DOCTYPE html>');
+				});
+				it('should NOT complain env', function () {
+					expect(response.text).not.to.contain('You still need to set the <span class="label label-default">AWS_ACCESS_KEY_ID</span> ENV variable.');
+					expect(response.text).not.to.contain('You still need to set the <span class="label label-default">AWS_SECRET_ACCESS_KEY</span> ENV variable.');
+				});
+				it('should say login was successful', function () {
+					expect(response.text).to.contain('App is running and has access to Amazon S3!');
+				});
+			});
+		});
+		
+		describe('and credentials are wrong', function () {
+			before(function () {
+				AWS.S3 = function () {
+					return {
+						listBuckets: function (callback) {
+							callback(true);
+						}
+					};
+				};
+			});
+			
+			describe('and accessing index page', function () {
+				before(function (done) {
+					request(app)
+						.get('/')
+						.end(function (err, res) {
+							response = res;
+							done();
+						})
+					;
+				});
+				
+				it('should respond with success', function () {
+					expect(response.status).to.equal(200);
+				});
+				it('should render', function () {
+					expect(response.text).to.contain('<!DOCTYPE html>');
+				});
+				it('should NOT complain env', function () {
+					expect(response.text).not.to.contain('You still need to set the <span class="label label-default">AWS_ACCESS_KEY_ID</span> ENV variable.');
+					expect(response.text).not.to.contain('You still need to set the <span class="label label-default">AWS_SECRET_ACCESS_KEY</span> ENV variable.');
+				});
+				it('should say login is failing', function () {
+					expect(response.text).to.contain('Access to Amazon S3 was denied. Are you sure the credentials are correct?');
+				});
+			});
+		});
+		
+		after(function () {
+			delete process.env.AWS_ACCESS_KEY_ID;
+			delete process.env.AWS_SECRET_ACCESS_KEY;
+		});
+	});
+	
 })
